d740278cf1b340c6dc380651fc84a16144823b7f
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-16 13:04:40 +0530

    [OH_A2_03] Not Found Section- Added 404 Not found and Internal Error routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from '@core/guards/auth-guard/auth.guard';
 import { unauthGuard } from '@core/guards/unauth-guard/unauth.guard';
+import { ErrorComponent } from '@features/error/error.component';
 
 const routes: Routes = [
     {
@@ -24,6 +25,28 @@ const routes: Routes = [
                 (module) => module.DashboardModule,
             ),
         canActivate: [unauthGuard],
+    },
+    {
+        path: 'error',
+        component: ErrorComponent,
+        data: {
+            label: 'Something has gone seriously wrong',
+            imgSrc: 'assets/images/InternalError.webp',
+            imgAlt: 'Internal Error',
+            message:
+                'It’s always time for a coffee break We should be back by the time you finish your coffee.',
+        },
+    },
+    {
+        path: '**',
+        component: ErrorComponent,
+        data: {
+            label: 'Page not Found',
+            imgSrc: 'assets/images/404NotFound.webp',
+            imgAlt: 'Page not Found',
+            message:
+                'Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us.',
+        },
     },
 ];
 
