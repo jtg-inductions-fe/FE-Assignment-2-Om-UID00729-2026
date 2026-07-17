7e1d6b8dedb83b5c2a617023370bf75fc94c8c2e
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-17 19:07:49 +0530

    [OH_A2_00] Base-Setup Review Fixes- namespacing, folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
