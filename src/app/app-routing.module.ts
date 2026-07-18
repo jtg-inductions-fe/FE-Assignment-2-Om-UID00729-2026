601a9bce8f921bc691b17cb2b5073306a66afe3f
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-18 19:52:58 +0530

    [OH_A2_02]Header section- Fixed paddings and namespaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authGuard } from '@core/guards/auth.guard';
+import { authGuard } from '@core/guards/auth-guard/auth.guard';
 
 const routes: Routes = [
     {
