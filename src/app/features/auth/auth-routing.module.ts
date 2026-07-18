38bc5d4f596f8cd239e5d6ce9ae3cbc2115fc944
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-18 23:12:55 +0530

    [OH_A2_01] Login Section- Changed auth feature folder structure

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
