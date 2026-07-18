fe63185bde808b0b439183677848115c5260fc8d
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-19 00:31:40 +0530

    [OH_A2_03]Not Found section- Updated error route handling

--- src/app/core/services/error-handler.service.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorHandler, inject } from '@angular/core';
-import { Router } from '@angular/router';
-
-export class ErrorHandlerService extends ErrorHandler {
-    router = inject(Router);
-    override handleError(): void {
-        this.router.navigate(['/error']);
-    }
-}
