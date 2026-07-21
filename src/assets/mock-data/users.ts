03b21b7f8e87975e750c035b23b1df1259a77e9d
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-21 15:19:57 +0530

    [OH_A2_01]Login- Corrected path for profile image in mock data

--- src/assets/mock-data/users.ts
+++ src/assets/mock-data/users.ts
@@ -6,20 +6,20 @@ export const mockUsers: authModel[] = [
         email: 'admin@jtg.com',
         password: 'admin123',
         role: 'admin',
-        profileImg: '@assets/images/profile-placeholder-1.webp',
+        profileImg: 'assets/images/profile-placeholder-1.webp',
     },
     {
         id: 2,
         email: 'pizzapalace@jtg.com',
         password: 'pizza123',
         role: 'owner',
-        profileImg: '@assets/images/profile-placeholder-2.webp',
+        profileImg: 'assets/images/profile-placeholder-2.webp',
     },
     {
         id: 3,
         email: 'burgerbistro@jtg.com',
         password: 'burger123',
         role: 'owner',
-        profileImg: '@assets/images/profile-placeholder-3.webp',
+        profileImg: 'assets/images/profile-placeholder-3.webp',
     },
 ];
