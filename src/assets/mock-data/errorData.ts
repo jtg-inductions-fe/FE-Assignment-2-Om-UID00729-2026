e33c0d19bfe188aa48538475cadf26329f41913b
Om Handa <om.handa@joshtechnologygroup.com>
2026-07-22 19:16:04 +0530

    [OH_A2_03]Not-Found-  Updated errordata

---
+++ src/assets/mock-data/errorData.ts
@@ -0,0 +1,16 @@
+export const errorData = {
+    'Internal Error': {
+        label: 'Something has gone seriously wrong',
+        imgSrc: 'assets/images/InternalError.webp',
+        imgAlt: 'Internal Error',
+        message:
+            'It’s always time for a coffee break We should be back by the time you finish your coffee.',
+    },
+    'Not-Found': {
+        label: 'Page not Found',
+        imgSrc: 'assets/images/404NotFound.webp',
+        imgAlt: 'Page not Found',
+        message:
+            'Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us.',
+    },
+};
